d84e34cb9a666c23f595cbaf56266c807d7d9782
arkab24 <arkab.24@gmail.com>
2025-01-01 02:59:11 -0800

    Update MonthlyProgressIndicator.tsx

--- src/components/MonthlyProgressIndicator.tsx
+++ src/components/MonthlyProgressIndicator.tsx
@@ -30,7 +30,7 @@ export const MonthlyProgressIndicator = ({
             }`}
           >
             <span className="text-xs text-breath-text mb-1">
-              {format(month, 'L')}
+              {format(month, 'M')}
             </span>
             <div
               className={`w-6 h-6 rounded-full flex items-center justify-center ${
